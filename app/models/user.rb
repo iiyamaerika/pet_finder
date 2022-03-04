645d1ce086d638455a835e5343183611a0d40181
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-04 01:53:50 +0000

    [Add]事前に作成したER図、テーブル定義書をもとにアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :articles, dependent: :destroy
+  
+  # いいね・コメント機能
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  
+  # フォロー・フォロワー機能
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  #DM機能
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  
+  #バリデーション
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  
+
 end
