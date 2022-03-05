63eab7ec44f27a91aea18387dae0d877ac93c74f
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-05 20:53:02 +0000

    [Add]ヘッダー、フッター

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about"=>"homes#about",as: 'about'
   
-  resources :users, only: [:show,:edit,:update]do
+  resources :users, only: [:show, :edit, :update]do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
-  resources :articles, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resource :favorites,only:[:create, :destroy]
+  resources :articles, only: [:index, :show, :edit, :new, :create, :destroy, :update] do
+    resource :favorites,only:[:create, :destroy, :show]
     resources :post_comments,only:[:create, :destroy]
   end
   
