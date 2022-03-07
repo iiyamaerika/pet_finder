f420fee84b1cfc8f1a060ba5edd3386711b375be
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-07 04:13:31 +0000

    [Add]新規投稿フォーム

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,37 @@
 class ArticlesController < ApplicationController
+  
   def index
   end
 
   def show
   end
 
-  def edit
+  def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(articke_params)
+    if @article.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
   end
 
-  def destroy
+  def edit
   end
 
   def update
   end
+
+  def destroy
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:status, :image, :title, :date, :prefecture, :place, :animal_type, :animal_age, :animal_sex, :introduction).merge(user_id: current_user.id)
+  end
+  
 end
