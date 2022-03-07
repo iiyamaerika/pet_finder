01df4ae374e7cf827a6673ecac7d3ca0b173cd92
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-07 04:13:31 +0000

    [Add]新規投稿フォーム

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ApplicationRecord
-  
+
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites_users, through: :favorites, source: :user
-  
+
+
+  enum status: { lost: 0, protection: 1, foster_homes: 2, solved: 3 }
+  enum animal_sex: { male: 0, female: 1, unknown: 2 }
+
 end
