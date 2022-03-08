9cc4c9b8a601a531cfea1ee667d544986d94a34d
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-08 22:53:55 +0000

    [Add]ユーザー情報編集画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :telephone_number, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :name_kana, :telephone_number, :address])
   end
   
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+      if @user != current_user
+        redirect_to user_path(current_user)
+      else
+        render "edit"
+      end
   end
 
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "会員情報を変更しました。."
+    else
+      render "edit"
+    end
   end
 end
