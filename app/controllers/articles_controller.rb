6611400ff5c205a3c8fe9f529b19dc3f04f28bd4
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-08 06:24:25 +0000

    [Fix]articleコントローラー

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy]
 
   def index
     @articles = Article.all
@@ -14,6 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     article = Article.new(article_params)
+    article.user_id = current_user.id
     if article.save
       redirect_to article_path(article.id)
     else
@@ -28,7 +31,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      redirect_to article_path(@article), notice: "You have updated book successfully."
+      redirect_to article_path(@article),
+      notice: "権限がありません。"
     else
       render "edit"
     end
@@ -43,7 +47,14 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:status, :image, :title, :date, :prefecture, :place, :animal_type, :animal_sex, :animal_age, :animal_breed, :introduction).merge(user_id: current_user.id)
+    params.require(:article).permit(:status, :image, :title, :date, :prefecture, :place, :animal_type, :animal_sex, :animal_age, :animal_breed, :introduction)
+  end
+
+  def ensure_correct_user
+    @article = Article.find(params[:id])
+    unless @article.user == current_user
+      redirect_to articles_path
+    end
   end
 
 end
