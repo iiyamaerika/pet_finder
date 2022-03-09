7f23971c93b3360f4514bad5b1e85b08741b531e
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-09 08:15:35 +0000

    [Add]コメント機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,21 @@
 class PostCommentsController < ApplicationController
   def create
+    article = Article.find(params[:article_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.article_id = article.id
+    comment.save
+    redirect_to article_path(article)
   end
 
   def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to article_path(params[:article_id])
   end
+  
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+  
 end
