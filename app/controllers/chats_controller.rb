d864e5772b274829bb1c6af81035a52b8163a81f
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-18 07:55:34 +0000

    [Add] DM通知機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -18,6 +18,7 @@ class ChatsController < ApplicationController
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
+    
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
@@ -25,6 +26,39 @@ class ChatsController < ApplicationController
   def create
     @chat = current_user.chats.new(chat_params)
     render :validater unless @chat.save
+      
+    
+    #通知機能
+    @user = User.find(params[:id])
+    rooms = current_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    
+    @room = user_rooms.room
+    @room_member = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    notification = current_user.passive_notifications.new(
+        room_id: @room.id,
+        message_id: @chat.id,
+        visited_id: @room_member.user_id,  #DM相手
+        visitor_id: current_user.id,
+        action: 'dm'
+        )
+    
+    if notification.visitor_id == notification.visited_id
+      notification.checked = false
+    else
+      @chat.save
+      notification.checked = true
+    end
+    
+    # if UserRoom.where(user_id: current_user.id, room_id: params[:chat][:room_id]).present?
+    #   @new_chat = Chat.create(params.require(:chat).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
+    #   @room = @new_chat.room
+    #   @room.create_notification_dm!(current_user, @new_chat.id)
+    #   rredirect_to request.referer
+    # else
+    #   redirect_back(fallback_location: root_path)
+    # end
+    
   end
   
   private
