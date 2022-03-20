7b4ac318c6200de5b8494b5f9d413b35da240b8f
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-20 10:18:46 +0000

    [Add]トップページレイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,9 @@
 class HomesController < ApplicationController
-  
+
   # 新着記事として最初の５件表示
   def top
-    @articles = Article.with_attached_image.includes([:image_attachment]).order(created_at: :desc).first(5)
+    @articles = Article.with_attached_image.includes([:image_attachment]).order(created_at: :desc).first(2)
+    @map_articles = Article.all
   end
-  
+
 end
