bdbded7edbedc6904964d99432ba7d22f7eee2e5
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-21 04:31:58 +0000

    [Fix]掲示板、お問合せ、コメント、検索、施設一覧まわりのレイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 
   # 新着記事として最初の５件表示
   def top
-    @articles = Article.with_attached_image.includes([:image_attachment]).order(created_at: :desc).first(2)
+    @articles = Article.with_attached_image.includes([:image_attachment]).order(created_at: :desc).first(3)
     @map_articles = Article.all
   end
 
