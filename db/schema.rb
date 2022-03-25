115232c78c1877f1b66feffa0f0192aa15e40186
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-25 01:53:50 +0000

    [Fix]フッターのフォント変更

--- db/schema.rb
+++ db/schema.rb
@@ -135,7 +135,6 @@ ActiveRecord::Schema.define(version: 2022_03_16_123009) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "category", null: false
     t.string "name", null: false
     t.string "name_kana", null: false
     t.string "nickname", null: false
@@ -145,6 +144,7 @@ ActiveRecord::Schema.define(version: 2022_03_16_123009) do
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
