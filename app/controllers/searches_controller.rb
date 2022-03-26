1fad4afe5c0ae235bb4afa838b51d119c86aa384
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-03-26 03:18:50 +0000

    [Fix]検索結果ダブりの症状

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def search
     @q = Article.ransack(params[:q])
-    @search_articles = @q.result(distinct: true).with_attached_image.includes([:image_attachment]).order(created_at: :desc).page(params[:page]).per(3)
+    @search_articles = @q.result(distinct: true).with_attached_image.includes([:image_attachment]).order(created_at: :desc).page(params[:page]).per(9)
     # 検索結果は最新順
   end
 end
